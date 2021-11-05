b6f965ea9ca67eff80062bb812cf1895a2986a0b
pngzfoo <tanylap_sath@hotmail.com>
2021-11-06 00:00:04 +0700

    UI and toast

--- app/src/main/java/com/example/kotlinworkshopcmdev/MainActivity.kt
+++ app/src/main/java/com/example/kotlinworkshopcmdev/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.kotlinworkshopcmdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickLogin(view: View){
+        Toast.makeText(applicationContext,"Login",Toast.LENGTH_LONG).show()
+    }
+
+
+    fun onClickRegister(view:View){
+        Toast.makeText(applicationContext,"Register",Toast.LENGTH_LONG).show()
     }
 }
