a54665a41b01789305633ddc6d47a3878a527066
pngzfoo <tanylap_sath@hotmail.com>
2021-11-18 01:28:39 +0700

    Done #5 Load array data for recyclerView

--- app/src/main/java/com/example/kotlinworkshopcmdev/DetailActivity.kt
+++ app/src/main/java/com/example/kotlinworkshopcmdev/DetailActivity.kt
@@ -2,7 +2,13 @@ package com.example.kotlinworkshopcmdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -20,5 +26,29 @@ class DetailActivity : AppCompatActivity() {
         //Toast.makeText(applicationContext,"Username = ${username}, Password = ${password}", Toast.LENGTH_LONG).show()
         //show toast using parcelable
         Toast.makeText(applicationContext,"Username = ${acc?.username}, Password = ${acc?.password}", Toast.LENGTH_LONG).show()
+
+        //we have to set adapter when we used recyclerview
+        mRecyclerView.adapter = CustomAdapter()
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    inner class CustomAdapter : RecyclerView.Adapter<CustomHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomHolder {
+              val view = LayoutInflater.from(applicationContext).inflate(R.layout.list_item,parent,false)
+              return CustomHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CustomHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int { // set total row
+            return 20
+        }
+
+    }
+    inner class CustomHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
     }
 }
+
