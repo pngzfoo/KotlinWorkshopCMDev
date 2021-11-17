b46dfadbc5010750665e7d3bd6123e8d059b320a
pngzfoo <tanylap_sath@hotmail.com>
2021-11-18 01:51:55 +0700

    Done #6 Load recyclerView use loop mock up data for recycler view

--- app/src/main/java/com/example/kotlinworkshopcmdev/DetailActivity.kt
+++ app/src/main/java/com/example/kotlinworkshopcmdev/DetailActivity.kt
@@ -9,8 +9,12 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class DetailActivity : AppCompatActivity() {
+
+    //mock up data
+    var mDataArray = arrayListOf<ItemBean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +31,21 @@ class DetailActivity : AppCompatActivity() {
         //show toast using parcelable
         Toast.makeText(applicationContext,"Username = ${acc?.username}, Password = ${acc?.password}", Toast.LENGTH_LONG).show()
 
-        //we have to set adapter when we used recyclerview
+        addDummyData()
+
+        //we have to set adapter when we used recyclerview to show sth from an array
         mRecyclerView.adapter = CustomAdapter()
         mRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun addDummyData(){
+
+        for(i in 1..100){
+            mDataArray.add(ItemBean("Cherprang BNK48", "$i : Cherprang Areekul",""))
+        }
+        //mRecyclerView.adapter!!.notifyDataSetChanged() // refresh /it's work in the tutorial but i'm not
+
+
     }
 
     inner class CustomAdapter : RecyclerView.Adapter<CustomHolder>(){
@@ -40,15 +56,22 @@ class DetailActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(holder: CustomHolder, position: Int) {
 
+            //use this function to update each round
+            holder.title.text = mDataArray[position].title
+            holder.subtitle.text = mDataArray[position].subtitle
+
         }
 
         override fun getItemCount(): Int { // set total row
-            return 20
+            //return 20
+            return mDataArray.size
         }
 
     }
     inner class CustomHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-
+        var title = itemView.mTitleTextView
+        var subtitle = itemView.mSubtitileTextView
+        var image = itemView.mYoutubeImageView
     }
 }
 
---
+++ app/src/main/java/com/example/kotlinworkshopcmdev/ItemBean.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinworkshopcmdev
+
+data class ItemBean (var title : String?, var subtitle : String?, var image:String?) {
+
+}
