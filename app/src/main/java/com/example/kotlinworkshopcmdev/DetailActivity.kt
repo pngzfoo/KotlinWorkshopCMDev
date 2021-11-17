a84be49ebc76a1ebee9d0a9530257b5e097f27f2
pngzfoo <tanylap_sath@hotmail.com>
2021-11-17 16:03:37 +0700

    sent data from main activity to detail activity Done #2 start activity and share data with intent extra

--- app/src/main/java/com/example/kotlinworkshopcmdev/DetailActivity.kt
+++ app/src/main/java/com/example/kotlinworkshopcmdev/DetailActivity.kt
@@ -2,11 +2,18 @@ package com.example.kotlinworkshopcmdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        //recieve data from main activity
+        val username = intent.getStringExtra("username")
+        val password = intent.getStringExtra("password")
+
+        Toast.makeText(applicationContext,"Username = $username, Password = $password", Toast.LENGTH_LONG).show()
     }
 }
